beedb13fc200f515673793cb7bc38676b4828c26
unknown <chansalonoy96@gmail.com>
2025-03-10 15:56:51 +0800

    Fix price formatting and radio selectors

--- data/cart.js
+++ data/cart.js
@@ -2,7 +2,7 @@ export const cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
 },{
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    productId: '77a845b1-16ed-4eac-bdf9-5b591882113d',
     quantity: 1,
 }];
 
@@ -13,8 +13,8 @@ export function addToCart(productId){
     if(productId === cartItem.productId){
       matchingItem = cartItem;
     }
-    console.log(cartItem);
   });
+  console.log(matchingItem);
   if(matchingItem){
     matchingItem.quantity += 1;
   } else {
